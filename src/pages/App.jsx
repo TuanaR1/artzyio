215b0c0016757d9d2d5fa51155af65f44e297448
tuanarada <tuanarada@gmail.com>
2026-05-11 02:40:11 +0200

    Plotesimet ne fajlla

--- src/pages/App.jsx
+++ src/pages/App.jsx
@@ -1,24 +1,42 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import Navbar from "../components/Navbar";
 import Hero from "../components/Hero";
-import Gallery from "../components/Gallery"; 
-import Footer from "../components/Footer"; 
-
-import Bestsellers from "../components/Bestsellers"; 
+import Gallery from "../components/Gallery";
+import Bestsellers from "../components/Bestsellers";
+import Footer from "../components/Footer";
+import Contact from "./Contact";
+import About from "./About";
+import Products from "./Products";
+import Login from "./Login";
+import Signup from "./Signup";
 
 import "./App.css";
 
-function App() {
-  return (
-    <div className="app-container">
-      <Navbar />
-      <Hero />
-      <Gallery />
-      <Bestsellers />
-      <Footer />
-    </div>
-    
-  );
+function HomePage() {
+  return (
+    <div className="app-container">
+      <Navbar />
+      <Hero />
+      <Gallery />
+      <Bestsellers />
+      <Footer />
+    </div>
+  );
 }
 
-export default App;  
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/products" element={<Products />} />
+      <Route path="/contact" element={<Contact />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
+    </Routes>
+  );
+}
+
+export default App;
